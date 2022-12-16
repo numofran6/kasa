94e7c25611e87383592f0311ab2a94a70d2101f7
🎈 <numofran6@gmail.com>
2022-12-16 12:59:52 +0000

    0/1

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import UserProvider from './utils/User';
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
-
-if ((process.env.NODE_ENV = 'production')) disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
