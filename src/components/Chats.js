fb454f9d2d8fcccad4d9285c514e57499f44f65f
🎈 <numofran6@gmail.com>
2022-12-18 20:36:07 +0000

    0/5

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -29,22 +29,23 @@ export const Chats = () => {
 
 	return (
 		<div className="chats">
-			{Object.entries(chats)
-				?.sort((a, b) => b[1].date - a[1].date)
-				.map((chat) => (
-					<div
-						className="userChat"
-						key={chat[0]}
-						onClick={() => handleSelect(chat[1].userInfo)}
-					>
-						<img src={chat[1].userInfo.photoURL} alt="" />
+			{chats &&
+				Object.entries(chats)
+					?.sort((a, b) => b[1].date - a[1].date)
+					.map((chat) => (
+						<div
+							className="userChat"
+							key={chat[0]}
+							onClick={() => handleSelect(chat[1].userInfo)}
+						>
+							<img src={chat[1].userInfo.photoURL} alt="" />
 
-						<div className="userChatInfo">
-							<span>{chat[1].userInfo.displayName}</span>
-							<p>{chat[1].lastMessage?.text}</p>
+							<div className="userChatInfo">
+								<span>{chat[1].userInfo.displayName}</span>
+								<p>{chat[1].lastMessage?.text}</p>
+							</div>
 						</div>
-					</div>
-				))}
+					))}
 		</div>
 	);
 };
